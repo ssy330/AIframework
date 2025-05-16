ba58b6e765779e3af5ce6d506f72f432e33c616c
ssy330 <gimsoyeon092@gmail.com>
2025-05-17 06:04:21 +0900

    step07-13

--- practice.py
+++ practice.py
@@ -2,16 +2,49 @@ import numpy as np
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다. '.format(type(data)))
+            
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs, )
+
+            for x, gx in zip(f.inputs, gxs):
+                x.grad = gx
+
+                if x.creator is not None:
+                    funcs.append(x.creator)
 
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(y)
-        self.input = input
-        return output
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys, )
+        outputs = [Variable(as_array(y)) for y in ys]
+        
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+
+        return outputs if len(outputs) > 1 else outputs[0]
     
     def forward(self, x):
         raise NotImplementedError()
@@ -25,7 +58,7 @@ class Square(Function):
         return y
     
     def backward(self, gy):
-        x = self.input.data
+        x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
 
@@ -39,6 +72,14 @@ class Exp(Function):
         gx = np.exp(x) * gy
         return gx
 
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
+
 def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(x.data - eps)
     x1 = Variable(x.data + eps)
@@ -46,20 +87,25 @@ def numerical_diff(f, x, eps=1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
 
+def square(x):
+    return Square()(x)
 
-A = Square()
-B = Exp()
-C = Square()
+def exp(x):
+    return Exp()(x)
 
-x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
+def add(x0, x1):
+    return Add()(x0, x1)
 
-y.grad = np.array(1.0)
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
-print(b.grad)
-print(a.grad)
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+x = Variable(np.array(2))
+y = Variable(np.array(3))
+
+z = add(square(x), square(y))
+z.backward()
+print(z.data)
 print(x.grad)
+print(y.grad)
